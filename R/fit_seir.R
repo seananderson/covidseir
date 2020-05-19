d6ade3d04eaf1aef2835bc086d5eef950086cd03
Sean Anderson <sean@seananderson.ca>
2020-05-19 12:38:15 -0700

    Increase default time increment for speed

--- R/fit_seir.R
+++ R/fit_seir.R
@@ -143,7 +143,7 @@
 fit_seir <- function(daily_cases,
                      obs_model = c("NB2", "Poisson"),
                      forecast_days = 0,
-                     time_increment = 0.2,
+                     time_increment = 0.25,
                      samp_frac_fixed = NULL,
                      samp_frac_type = c("fixed", "estimated", "rw", "segmented"),
                      samp_frac_seg = NULL,
