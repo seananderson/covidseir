3a2526f88bd9fc83ffc7a8f580ce15b4a988d04c
carolinecolijn <ccolijn@gmail.com>
2020-07-30 15:07:53 -0700

    infectivity profile in Rt code

--- R/get_rt.R
+++ R/get_rt.R
@@ -17,7 +17,7 @@
 #' @details
 #' See [project_seir()] for an example.
 
-get_rt <- function(obj, lag = 30, ff = 1.25, forecast_days = 0,
+get_rt <- function(obj, lag = 30, forecast_days = 0,
   iter = seq_along(obj$post$R0)) {
   p <- project_seir(obj, forecast_days = forecast_days,
     iter = iter, return_states = TRUE)
@@ -25,37 +25,34 @@ get_rt <- function(obj, lag = 30, ff = 1.25, forecast_days = 0,
   temp <- dplyr::group_by(states, .iteration)
   temp <- dplyr::group_split(temp)
   temp <- purrr::map_dfr(temp,
-    get_rt_one_iteration, pars = obj$pars, lag = lag, ff = ff)
+    get_rt_one_iteration, pars = obj$pars, lag = lag)
   states$Rt <- temp$Rt
   states
 }
 
-get_rt_one_iteration <- function(states, pars, lag = 30, ff = 1.25) {
-  lag_avail <- states$time - min(states$time)
+get_rt_one_iteration <-function(states, pars, lag=30) {
+  lag_avail<- states$time-min(states$time)
   inds <- which(lag_avail >= lag)
-  dt <- states$time[2] - states$time[1]
-  numback <- lag / (dt) - 1
+  dt<-states$time[2]-states$time[1]
+  numback <- lag/(dt)-1
   denominator <- vector(length = nrow(states))
-  # eff rate for infs class:
-  partrate <- 1 / (1 / pars[["k2"]] + 1 / (1 / pars[["D"]] + pars[["q"]]))
-  # NOTE approx b/c duration in infectious class is not exponential.
-  inc_e2 <- pars[["k1"]] * (states$E1 + states$E1d)
+  
+  inc_e2<- pars["k1"] * (states$E1+states$E1d)
+  
   # have experienced that it doesn't matter which incident compartment we use
   # and this doesn't require reading in posterior time-dep f vals, and R0s
   for (i in inds) {
-    pastinds <- i - (1:numback)
-    pastcases <- inc_e2[pastinds]
-    s <- seq(from = 0, by = dt, length.out = length(pastinds))
-    # note ff (fudge factor) accounting for infectivity kernel not
-    # being quite right:
-    integrand <- pastcases *
-      convodens(rate1 = pars[["k1"]], rate2 = ff * partrate, .t = s)
-    trapezoid_weights <- rep(1, length(integrand))
-    trapezoid_weights[1] <- 0.5
-    trapezoid_weights[length(trapezoid_weights)] <- 0.5
-    denominator[i] <- dt * sum(integrand * trapezoid_weights)
+    pastinds <- i-(1:numback)
+    pastcases <-  inc_e2[pastinds] 
+    s <- seq(from=0, by=dt, length.out = length(pastinds))
+    integrand <- pastcases*infkernel(rate1=pars[["k1"]], rate2=pars[["k2"]],
+                                         rate3=pars[["q"]]+1/pars[["D"]],.t=s)
+    trapezoid_weights <- rep(1,length(integrand))
+    trapezoid_weights[1] <- 0.5 
+    trapezoid_weights[length(trapezoid_weights)]<-0.5
+    denominator[i] <- dt*sum(integrand*trapezoid_weights)
   }
-  denominator[which(denominator == 0)] <- NA
+  denominator[which(denominator==0)]<-NA
   dplyr::tibble(Rt = inc_e2 / denominator)
 }
 
@@ -70,4 +67,17 @@ convodens <- function(rate1, rate2, .t) {
     return(rate1 * rate2 * exp(-rate2 * .t) *
         (exp((rate2 - rate1) * .t) - 1) / (rate2 - rate1))
   }
+}
+
+# @param rate1 rate of first exponential (here, k1)
+# @param rate2 rate of second exponential (here, k2) 
+# @param rate3 rate of third exponential (here, rate leaving infectious class)
+# @param t time
+# @return infectivity kernel based density 
+infkernel = function(rate1, rate2, rate3, .t) {
+  if (rate1 == rate2 | rate2==rate3 | rate3==rate1) stop("rates cannot be identical")
+  return( (rate1*rate2*rate3/(rate2+rate3))*
+            (  exp(-rate1* .t)*(rate2+rate3 -rate1)/ ((rate1-rate2)*(rate1-rate3)) +
+                 exp(-rate2* .t)*rate3/((rate1-rate2)*(rate3-rate2)) +
+                 exp(-rate3* .t)*rate2/((rate1-rate3)*(rate2-rate3))))
 }
