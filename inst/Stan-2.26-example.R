f8a7c20de356b33fd9c67ee6ecce081fe29d055f
Sean Anderson <sean@seananderson.ca>
2021-03-24 13:51:58 -0700

    Add 2.26 example

---
+++ inst/Stan-2.26-example.R
@@ -0,0 +1,25 @@
+remove.packages(c("StanHeaders", "rstan"))
+install.packages("StanHeaders", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+install.packages("rstan", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+
+library(rstan)
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores() / 2)
+library(covidseir)
+
+stan_mod <- rstan::stan_model(system.file("stan", "seir.stan", package = "covidseir"))
+
+cases <- c(
+  0, 0, 1, 3, 1, 8, 0, 6, 5, 0, 7, 7, 18, 9, 22, 38, 53, 45, 40,
+  77, 76, 48, 67, 78, 42, 66, 67, 92, 16, 70, 43, 53, 55, 53, 29,
+  26, 37, 25, 45, 34, 40, 35
+)
+s1 <- c(rep(0.1, 13), rep(0.2, length(cases) - 13))
+m <- fit_seir(
+  cases,
+  stan_model = stan_mod,
+  iter = 200,
+  fit_type = "optimizing",
+  samp_frac_fixed = s1
+)
+print(m)
